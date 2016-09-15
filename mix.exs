b0bf022b025e3cf83890d9e411c83fef3d9f5b1d
Michael Truog <mjtruog@gmail.com>
2016-09-14 20:36:03 -0700

    Version 1.5.3.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceDbPgsql do
 
   def project do
     [app: :cloudi_service_db_pgsql,
-     version: "1.5.1",
+     version: "1.5.3",
      language: :erlang,
      description: description,
      package: package,
@@ -20,7 +20,7 @@ defmodule CloudIServiceDbPgsql do
      {:pgsql,
       [git: "https://github.com/semiocast/pgsql.git",
        branch: "master"]},
-     {:cloudi_core, "~> 1.5.1"}]
+     {:cloudi_core, "~> 1.5.3"}]
   end
 
   defp description do
