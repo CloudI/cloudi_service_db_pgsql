816f3e1e3fe8fca3e11919d23c35c634a89cd8df
Michael Truog <mjtruog@protonmail.com>
2018-09-15 20:40:00 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,8 +6,23 @@ defmodule CloudIServiceDbPgsql do
 
   def project do
     [app: :cloudi_service_db_pgsql,
-     version: "1.7.3",
+     version: "1.7.4",
      language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
@@ -23,7 +38,7 @@ defmodule CloudIServiceDbPgsql do
   defp deps do
     [{:epgsql, "~> 4.0"},
      {:pgsql, "~> 26.0"},
-     {:cloudi_core, "~> 1.7.3"}]
+     {:cloudi_core, "~> 1.7.4"}]
   end
 
   defp description do
